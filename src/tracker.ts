77b7559d4f3e9ee23f489ebc249454fa7b1e10b0
David Whiting <davw@apache.org>
2020-10-22 19:39:34 +0200

    Refactor 1st pass

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,43 +1,50 @@
-type Slot = {
+
+type Note = {
     note: number | "---" | 'cont',
     fx?: {
         pulseWidth?: number,
         glide?: number
     },
-    drum?: 'KCK' | 'NSS' | 'SNR'
     vel?: number
 }
+type Drum = {
+    drum: "---" | 'KCK' | 'NSS' | 'SNR'
+    vel?: number
+}
+
+type Slot = Note | Drum
 
 const A0 = -12;
 const A0F = 55;
 
 const PatternSize = 64;
 
-function getNoteName({note, fx, vel, drum}: Slot) {
-    let name = "";
-    if (note === "---") { name = "---"; }
-    else if (note === 'cont') { name =  "&nbsp;&nbsp;&nbsp;"; }
-    else if (drum) { name = drum; }
-    else {
-        name = drum ? drum : ['A-', 'A#', 'B-', 'C-', 'C#', 'D-', 'D#', 'E-', 'F-', 'F#', 'G-', 'G#'][(note - A0) % 12] + Math.floor((note - A0) / 12)
+function textRepr(slot: Slot) {
+    function hex(v: number) { return Math.floor(v * 255).toString(16).toUpperCase().padStart(2,'0'); }
+    function noteName(v: number | "---" | "cont") {
+        switch (v) {
+            case "---":
+                return "---";
+            case "cont":
+                return "&nbsp;&nbsp;&nbsp;";
+            default:
+                return ['A-', 'A#', 'B-', 'C-', 'C#', 'D-', 'D#', 'E-', 'F-', 'F#', 'G-', 'G#'][(v - A0) % 12] + Math.floor((v - A0) / 12)
+        }
     }
-
-    if (fx && fx.pulseWidth) {
-        name = name + " w" + Math.floor(fx.pulseWidth * 255).toString(16).toUpperCase();
+    if ("drum" in slot) {
+        let string = slot.drum;
+        if (slot.vel) string +=" v" + hex(slot.vel);
+        return string;
+    } else {
+        let string = noteName(slot.note);
+        if (slot.fx && slot.fx.pulseWidth) string += " w" +hex(slot.fx.pulseWidth);
+        if (slot.fx && slot.fx.glide) string +=" g" + hex(slot.fx.glide);
+        if (slot.vel) string += " v" + hex(slot.vel);
+        return string;
     }
-
-    if (fx && fx.glide) {
-        name = name + " g" + Math.floor(fx.glide * 255).toString(16).toUpperCase();
-    }
-
-    if (vel) {
-        name = name + " v" + Math.floor(vel * 255).toString(16).toUpperCase();
-    }
-
-    return name;
 }
 
-function fill<T>(count: number, fn: (number) => T): T[] {
+function fill<T>(count: number, fn: (x: number) => T): T[] {
     return new Array(count).fill(undefined).map((x,i) => fn(i));
 }
 
@@ -45,30 +52,53 @@ function choose<T>(array: T[]): T {
     return array[Math.floor(Math.random() * array.length)]
 }
 
+function rndInt(max: number): number {
+    return Math.floor(Math.random() * max);
+}
+
 type Pattern = Slot[];
+type DrumPattern = Drum[];
+type SynthPattern = Note[];
 
-const minor = [0,2,3,5,7,8,10];
-const major = [0,2,4,5,7,9,11];
+const music = {
+    scales: {
+        major:[0,2,3,5,7,8,10],
+        minor:[0,2,4,5,7,9,11]
+    },
+    progressions: [
+        [1,1,1,1,6,6,6,6,4,4,4,4,3,3,5,5],
+        [1,1,1,1,6,6,6,6,1,1,1,1,6,6,6,6],
+        [4,4,4,4,5,5,5,5,1,1,1,1,1,1,3,3],
+        [1,1,6,6,4,4,5,5,1,1,6,6,3,3,5,5],
+        [5,5,4,4,1,1,1,1,5,5,6,6,1,1,1,1],
+        [6,6,6,6,5,5,5,5,4,4,4,4,5,5,5,5],
+        [1,1,1,1,3,3,3,3,4,4,4,4,5,5,5,5],
+        [6,6,6,6,4,4,4,4,1,1,1,1,1,1,5,5],
+        [1,1,1,1,1,1,1,1,4,4,4,4,4,4,4,4]
+    ],
+    chordTypes: {
+        triad: [0,2,4],
+        single: [0]
+    }
+};
 
-const progressions =[
-    [1,1,1,1,6,6,6,6,4,4,4,4,3,3,5,5],
-    [1,1,1,1,6,6,6,6,1,1,1,1,6,6,6,6],
-    [4,4,4,4,5,5,5,5,1,1,1,1,1,1,3,3],
-    [1,1,6,6,4,4,5,5,1,1,6,6,3,3,5,5],
-    [5,5,4,4,1,1,1,1,5,5,6,6,1,1,1,1],
-    [6,6,6,6,5,5,5,5,4,4,4,4,5,5,5,5],
-    [1,1,1,1,3,3,3,3,4,4,4,4,5,5,5,5],
-    [6,6,6,6,4,4,4,4,1,1,1,1,1,1,5,5],
-    [1,1,1,1,1,1,1,1,4,4,4,4,4,4,4,4]
-];
-let progression = progressions[0] ;
-const triad = [0,2,4];
-let key = Math.floor(Math.random()*12);
-let scale = minor;
+type Key = number & {"keyType": true}
+
+let state = {
+    key: rndInt(12) as Key,
+    scale: music.scales.minor,
+    progression: music.progressions[0],
+    patterns: [] as Pattern[]
+};
+
+// let key = Math.floor(Math.random()*12);
+// let scale = minor;
 
 
 const gen = {
     arp: () => {
+        const [progression, key, scale] = [state.progression, state.key, state.scale];
+
         const octave = choose([0,12,24]);
         const offset = choose([0,1,2]);
         const pwOffset = Math.floor(Math.random() * 8) * 2;
@@ -76,7 +106,7 @@ const gen = {
         return fill(PatternSize, i => {
             const progIndex = Math.floor(i / 4);
             const chordNumber = progression[progIndex];
-            const chord = triad.map(noteIndex => key + scale[(chordNumber - 1 + noteIndex) % scale.length]);
+            const chord = music.chordTypes.triad.map(noteIndex => key + scale[(chordNumber - 1 + noteIndex) % scale.length]);
             return {
                 note: chord[(i + offset + choose([0, 0, 0, 1, 2])) % chord.length] + octave + choose([0,12]),
                 fx: {
@@ -86,19 +116,22 @@ const gen = {
         })
     },
     bass: () => fill(PatternSize, i => {
+        const [progression, key, scale] = [state.progression, state.key, state.scale];
         const progIndex = Math.floor(i / 4);
         const chordNumber = progression[progIndex];
-        const chord = triad.map(noteIndex => key + scale[(chordNumber - 1 + noteIndex) % scale.length]);
+        const chord = music.chordTypes.single.map(noteIndex => key + scale[(chordNumber - 1 + noteIndex) % scale.length]);
         return {note: i % 2 === 1 ? 'cont' : chord[0] + (Math.floor(i / 2) % 2) * 12 - 12, fx: {pulseWidth: 0}} as Slot;
     }),
     bass2: () => fill(PatternSize, i => {
+        const [progression, key, scale] = [state.progression, state.key, state.scale];
         const progIndex = Math.floor(i / 4);
         const chordNumber = progression[progIndex];
-        const chord = triad.map(noteIndex => key + scale[(chordNumber - 1 + noteIndex) % scale.length]);
+        const chord = music.chordTypes.single.map(noteIndex => key + scale[(chordNumber - 1 + noteIndex) % scale.length]);
         return {note: i % 8 === 0 ? chord[0] - 12: 'cont', vel: 2, fx: {pulseWidth: Math.random()}} as Slot;
         //return {note: i % 2 === 1 ? 'cont' : chord[0] + (Math.floor(i / 2) % 2) * 12 - 12} as Slot;
     }),
     melody1: () => {
+        const [progression, triad, key, scale] = [state.progression, music.chordTypes.triad, state.key, state.scale];
         const slow = Math.random() < 0.5;
         const pwmMod = Math.random() < 0.5;
         let pwmAmount = Math.random() * 0.5;
@@ -152,50 +185,39 @@ const gen = {
                     pulseWidth: pwmMod ? pwmAmount : 0
                 }});
 
-            // const progIndex = Math.floor(i/4);
-            // const chordNumber = progression[progIndex];
-            // const chord = triad.map(noteIndex => key + scale[(chordNumber - 1 + noteIndex - 1) % scale.length]);
         }
         return pattern;
     },
-    empty: () => fill(PatternSize, i => ({note: '---'} as Slot)),
+    empty: () => fill(PatternSize, _ => ({note: '---'} as Slot)),
     drum: () => fill(PatternSize, i => ({note: 0, drum: i % 8 === 0 ? 'KCK' : i % 8 === 4 ? 'SNR' : (i % 2 === 0 && Math.random() < 0.2) ? 'KCK' : (Math.random() < 0.05) ? choose(['KCK', 'SNR']) : 'NSS', vel: 0.6 + 0.2 * (1-(i % 2)) } as Slot))
 };
 
-const display = document.getElementById("display");
 
-const patterns: Pattern[] = [];
+function createPatternDisplay(display: HTMLElement) {
+// const display = document.getElementById("display") as HTMLDivElement;
 
-function replacePatterns(newPats: Pattern[]) {
+    function replacePatterns(newPats: Pattern[]) {
 
-    patterns.length = 0;
-    display.innerHTML = "";
+        display.innerHTML = "";
 
-    let i = 0;
-    function add(pattern: Pattern, index: number) {
-        const pDisplay = document.createElement("code");
-        pDisplay.innerHTML =
-            "<h3>" + (index === 4 ? "*" : "⎍") + (index + 1) + "</h3>" +
-            pattern.map((x, i) => "<div class='note' data-index='" + i + "'>" + getNoteName(x) + "</div>").join("");
+        function add(pattern: Pattern, index: number) {
+            const pDisplay = document.createElement("code");
+            pDisplay.innerHTML =
+                "<h3>" + (index === 4 ? "*" : "⎍") + (index + 1) + "</h3>" +
+                pattern.map((x, i) => "<div class='note' data-index='" + i + "'>" + textRepr(x) + "</div>").join("");
 
-        display.append(pDisplay);
-        patterns.push(pattern);
+            display.append(pDisplay);
+        }
 
+        newPats.forEach((p, i) => add(p, i))
     }
-    newPats.forEach((p, i) => add(p, i))
-    //
-    // add(gen.bass());
-    // add(gen.arp());
-    // add(gen.melody1());
-    // add(gen.arp());
-}
 
-function rowHighlighter() {
     const customStyle = document.createElement("style");
     customStyle.setAttribute("type", "text/css");
     document.body.append(customStyle);
     const css = customStyle.sheet as CSSStyleSheet;
-    function highlight(index: number) {
+
+    function highlightRow(index: number) {
         if (css.rules.length > 0) {
             css.deleteRule(0);
         }
@@ -203,9 +225,11 @@ function rowHighlighter() {
     }
 
     return {
-        highlight
-    }
+        replacePatterns,
+        highlightRow
+    };
 }
+
 
 
 function SquareSynth(ctx: AudioContext, pan: number = 0) {
@@ -253,13 +277,13 @@ function SquareSynth(ctx: AudioContext, pan: number = 0) {
         gain.gain.cancelScheduledValues(ctx.currentTime);
         gain.gain.setTargetAtTime(0, ctx.currentTime, 0.01);
     }
-    function play(slot: Slot) {
+    function play(slot: Note) {
         if (slot.note === "---") {
             noteOff();
         }  else if (slot.note === 'cont') {
             // do nothing
         } else {
-            noteOn(slot.note, slot.fx.glide);
+            noteOn(slot.note, slot.fx?.glide);
         }
         if (slot.fx && slot.fx.pulseWidth) {
             width.cancelScheduledValues(ctx.currentTime);
@@ -301,43 +325,35 @@ function DrumSynth(ctx: AudioContext) {
     // gain.connect(analyser);
     // noiseGain.connect(analyser);
 
-    function play(slot: Slot) {
+    function play(slot: Drum) {
         const vel = slot.vel ? slot.vel : 1;
-        if (slot.note === "---") {
-
-        } else if (slot.note === 'cont') {
-
-        } else {
-            if (slot.drum === 'KCK') {
-                osc.detune.cancelScheduledValues(ctx.currentTime);
-                osc.detune.setValueAtTime(3000, ctx.currentTime);
-                osc.detune.setTargetAtTime(0, ctx.currentTime, 0.07);
-                gain.gain.cancelScheduledValues(ctx.currentTime);
-                gain.gain.setValueAtTime(0.3 * vel, ctx.currentTime);
-                gain.gain.setValueCurveAtTime([0.3 * vel, 0.3 * vel, 0.2 * vel, 0.1 * vel, 0.0], ctx.currentTime, 0.10);
-            } else if (slot.drum === 'NSS') {
-                noiseGain.gain.cancelScheduledValues(ctx.currentTime);
-                noiseGain.gain.setValueAtTime(0.1 * vel,ctx.currentTime);
-                noiseGain.gain.setValueCurveAtTime([0.1 * vel,0.04 * vel,0.0], ctx.currentTime, 0.08);
-                noisePan.pan.cancelScheduledValues(ctx.currentTime);
-                noisePan.pan.setValueAtTime(Math.random() * 0.4-0.2, ctx.currentTime);
-            } else if (slot.drum === 'SNR') {
-                osc.detune.cancelScheduledValues(ctx.currentTime);
-                osc.detune.setValueAtTime(2400, ctx.currentTime);
-                osc.detune.setTargetAtTime(600, ctx.currentTime, 0.04);
-                gain.gain.cancelScheduledValues(ctx.currentTime);
-                gain.gain.setValueAtTime(0.2 * vel, ctx.currentTime);
-                gain.gain.setValueCurveAtTime([0.2 * vel, 0.06 * vel, 0.02 * vel, 0], ctx.currentTime, 0.10);
-                noiseGain.gain.cancelScheduledValues(ctx.currentTime);
-                noiseGain.gain.setValueAtTime(0.2 * vel,ctx.currentTime);
-                noiseGain.gain.setValueCurveAtTime([0.2 * vel,0.15 * vel,0.0], ctx.currentTime, 0.15);
-            }
-            //gain.gain.setTargetAtTime(0.0,ctx.currentTime, 0.08);
+        if (slot.drum === 'KCK') {
+            osc.detune.cancelScheduledValues(ctx.currentTime);
+            osc.detune.setValueAtTime(3000, ctx.currentTime);
+            osc.detune.setTargetAtTime(0, ctx.currentTime, 0.07);
+            gain.gain.cancelScheduledValues(ctx.currentTime);
+            gain.gain.setValueAtTime(0.3 * vel, ctx.currentTime);
+            gain.gain.setValueCurveAtTime([0.3 * vel, 0.3 * vel, 0.2 * vel, 0.1 * vel, 0.0], ctx.currentTime, 0.10);
+        } else if (slot.drum === 'NSS') {
+            noiseGain.gain.cancelScheduledValues(ctx.currentTime);
+            noiseGain.gain.setValueAtTime(0.1 * vel,ctx.currentTime);
+            noiseGain.gain.setValueCurveAtTime([0.1 * vel,0.04 * vel,0.0], ctx.currentTime, 0.08);
+            noisePan.pan.cancelScheduledValues(ctx.currentTime);
+            noisePan.pan.setValueAtTime(Math.random() * 0.4-0.2, ctx.currentTime);
+        } else if (slot.drum === 'SNR') {
+            osc.detune.cancelScheduledValues(ctx.currentTime);
+            osc.detune.setValueAtTime(2400, ctx.currentTime);
+            osc.detune.setTargetAtTime(600, ctx.currentTime, 0.04);
+            gain.gain.cancelScheduledValues(ctx.currentTime);
+            gain.gain.setValueAtTime(0.2 * vel, ctx.currentTime);
+            gain.gain.setValueCurveAtTime([0.2 * vel, 0.06 * vel, 0.02 * vel, 0], ctx.currentTime, 0.10);
+            noiseGain.gain.cancelScheduledValues(ctx.currentTime);
+            noiseGain.gain.setValueAtTime(0.2 * vel,ctx.currentTime);
+            noiseGain.gain.setValueCurveAtTime([0.2 * vel,0.15 * vel,0.0], ctx.currentTime, 0.15);
         }
     }
     return {
         play,
-        //analyser
     }
 }
 
@@ -345,19 +361,21 @@ function DrumSynth(ctx: AudioContext) {
 function modulate() {
     choose([
         () => {
-            if (scale === minor) {
-                scale = major;
-                key = (key + 3) % 12;
-            } else if (scale === major) {
-                scale = minor;
-                key = (key + 9) % 12;
+            // Move to relative major or minor
+            if (state.scale === music.scales.minor) {
+                state.scale = music.scales.major;
+                state.key = (state.key + 3) % 12 as Key;
+            } else if (state.scale === music.scales.major) {
+                state.scale = music.scales.minor;
+                state.key = (state.key + 9) % 12 as Key;
             }
         },
         () => {
+            // Move around the cycle of fifths
             if (Math.random() < 0.5) {
-                key = (key + 7) % 12;
+                state.key = (state.key + 7) % 12 as Key;
             } else {
-                key = (key + 5) % 12;
+                state.key = (state.key + 5) % 12 as Key;
             }
         }
     ])();
@@ -365,11 +383,13 @@ function modulate() {
 
 function start() {
     let fN = 0;
-    const hl = rowHighlighter();
+    const display = createPatternDisplay(document.getElementById("display") as HTMLElement);
+
     let bpm = 112;
-    let handle = {timer: 0};
+    let intervalHandle = {timer: 0};
+
     // @ts-ignore
-    const ctx = new (window.AudioContext || window.webkitAudioContext)() as AudioContext;
+    const ctx: AudioContext = new (window.AudioContext || window.webkitAudioContext)() as AudioContext;
 
     const synths = [
         SquareSynth(ctx),
@@ -384,37 +404,37 @@ function start() {
         const positionInPattern = f % PatternSize;
         if (f % 1024 === 0) {
             bpm = Math.floor(Math.random() * 80) + 100;
-            window.clearInterval(handle.timer);
-            handle.timer = window.setInterval(doFrame, (60000/bpm)/4);
+            window.clearInterval(intervalHandle.timer);
+            intervalHandle.timer = window.setInterval(doFrame, (60000/bpm)/4);
         }
         if (f % 512 === 0) {
             modulate();
         }
         if (f % 256 === 0) {
-            progression = choose(progressions);
+            state.progression = choose(music.progressions);
         }
         if (f % 128 === 0) {
-            replacePatterns([
+            state.patterns =[
                 choose([gen.bass, gen.bass2, gen.empty])(),
                 Math.random() < 0.7 ? gen.arp() : gen.empty(),
                 Math.random() < 0.7 ? gen.melody1() : gen.empty(),
                 choose([gen.empty, gen.arp, gen.melody1])(),
                 Math.random() < 0.8 ? gen.drum() : gen.empty(),
-            ]);
+            ];
+            display.replacePatterns(state.patterns);
         }
-        hl.highlight(positionInPattern);
+        display.highlightRow(positionInPattern);
 
-        patterns.forEach((pat, i) => {
+        state.patterns.forEach((pat, i) => {
             const note = pat[positionInPattern];
-            synths[i].play(note);
-            // if (no
+            synths[i].play(note as any);
         });
     }
 
     function doFrame() {
         frame(fN++);
     }
-    handle.timer = window.setInterval(doFrame, (60000 / bpm) / 4);
+    intervalHandle.timer = window.setInterval(doFrame, (60000 / bpm) / 4);
 }
 
 let started = false;
